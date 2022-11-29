4f5a8c8a13d26cef92644e8313d531cfc44a5667
Mac <mac@Mars-MacBook-Pro.local>
2022-11-29 18:11:58 +0100

    k

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range((i+1), 10):
-        if (i is not 8) or (j is not 9):
-            print("{}{}, ".format(i, j), end="")
+for a in range(0, 10):
+    for b in range((a+1), 10):
+        if (a != 8) or (b != 9):
+            print("{}{}, ".format(a, b), end="")
         else:
-            print("{}{}".format(i, j))
+            print("{}{}".format(a, b))
