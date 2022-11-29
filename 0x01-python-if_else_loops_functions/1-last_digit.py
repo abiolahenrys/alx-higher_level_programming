65fae1435cb4bf36f9e0db2e20598b28a47aefa8
Mac <mac@Mars-MacBook-Pro.local>
2022-11-29 16:16:27 +0100

    L

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ if(number < 0):
 else:
     lat = int(str(number)[-1])
 if lat > 5:
-    print("Last digit of" ,number, "is", lat, "and is greater than 5")
+    print("Last digit of" , number , "is" , lat , "and is greater than 5")
 elif lat == 0:
-    print('Last digit of', number, 'is' ,lat, 'and is 0')
+    print('Last digit of' , number , 'is' , lat , 'and is 0')
 else:
-    print("Last digit of" ,number, "is", lat, "and is less than 6 and not 0")
+    print("Last digit of" , number , "is" , lat , "and is less than 6 and not 0")
